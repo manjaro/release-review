d3fb4f98cd0d4b6846d4d2ee7c773c1cdb1b9a80
Jonas Strassel <info@jonas-strassel.de>
2024-05-05 10:34:00 +0200

    :sparkles: Add i3 and mate profiles for arm

--- functions/[mode]/[search].ts
+++ functions/[mode]/[search].ts
@@ -1,19 +1,8 @@
 import z from "zod";
 
 const paramsValidator = z.object({
-  mode: z.enum(["nightly", "release"]),
-  arch: z.enum(["x86_64", "arm64"]),
-  branch: z.enum(["stable", "testing", "unstable"]),
-  edition: z.enum([
-    "xfce",
-    "gnome",
-    "kde",
-    "minimal",
-    "i3",
-    "cinnamon",
-    "mate",
-    "sway",
-  ]),
+  search: z.string(),
+  mode: z.enum(["release", "pre-release"]),
 });
 
 type Release = {
@@ -21,23 +10,32 @@ type Release = {
   created_at: string;
 };
 
+/**
+ * Github expects a User-Agent header to be set
+ */
 const ghInit = {
   headers: {
     "User-Agent": "release-review",
     Accept: "application/vnd.github+json",
   },
-};
+  cf: {
+    cacheTtl: 60 * 10,
+  },
+} satisfies RequestInit<RequestInitCfProperties>;
 
 let ghRepo = "manjaro-contrib/release-review";
 
 export const onRequest: PagesFunction = async (context) => {
-  const input = paramsValidator.safeParse(context.params);
+  const params = paramsValidator.safeParse(context.params);
 
-  if (!input.success) {
-    return Response.json("Invalid request", {
-      status: 400,
-      headers: { "content-type": "application/json" },
-    });
+  if (!params.success) {
+    return Response.json(
+      { error: params.error },
+      {
+        status: 400,
+        headers: { "content-type": "application/json" },
+      }
+    );
   }
 
   const cache = caches.default;
@@ -48,14 +46,14 @@ export const onRequest: PagesFunction = async (context) => {
     return response;
   }
 
-  if (input.data.edition === "sway") {
+  if (params.data.search.includes("sway")) {
     ghRepo = "manjaro-sway/manjaro-sway";
   }
 
   let release: Release;
 
   try {
-    if (input.data.mode === "release") {
+    if (params.data.mode === "release") {
       const result = await fetch(
         `https://api.github.com/repos/${ghRepo}/releases/latest`,
         ghInit
@@ -74,14 +72,9 @@ export const onRequest: PagesFunction = async (context) => {
     return Response.json("Not found", { status: 404 });
   }
 
-  console.log({ release: release.assets, input: input.data });
-
+  const regex = new RegExp(params.data.search, "i");
   const assets = release.assets
-    .filter(
-      (asset) => asset.name.includes("-ARM-") === (input.data.arch === "arm64")
-    )
-    .filter((asset) => asset.name.includes(`-${input.data.edition}-`))
-    .filter((asset) => asset.name.includes(`-${input.data.branch}-`))
+    .filter((asset) => regex.test(asset.name))
     .map((asset) => asset.browser_download_url);
 
   return Response.json(assets, {
